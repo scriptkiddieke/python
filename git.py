c72414595f51f19ab95a24e2943aef6814bc5d3f
scriptkiddieke <alexmwangimungai254@gmail.com>
2023-07-06 00:00:00 +0300

    Hack the contribution graph 1

--- git.py
+++ git.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta
 REPO_PATH = r'C:\Users\alexm\Desktop\py'
 COMMIT_MESSAGE = 'Hack the contribution graph'  
 START_DATE = '2023-07-06'  
-DAYS = 100
-DAYS_TO_SKIP = 1
+DAYS = 200
+DAYS_TO_SKIP = 0
 TEMP_FILE_NAME = 'temp.txt'  
 BRANCH_NAME = 'main'
 
